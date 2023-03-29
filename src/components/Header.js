27c5b45174e6731399e141efc8646f6fd60d9956
talzaken <talzaken@gmail.com>
2023-03-29 19:38:53 -0400

    add landing page and how to play page

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from '../styles/Header.module.css';
+
+export const Header = () => {
+  return (
+    <header className={styles.header}>
+      <div className={styles.container}>
+        <div className={styles.logo}>
+          <span className={styles.title}>Telephonish</span>
+        </div>
+        <nav className={styles.navbar}>
+          <a href="/" className={styles.navlink}>Home</a>
+          <a href="/how-to-play" className={styles.navlink}>How to Play</a>
+          <a href="/gameplay" className={styles.navlink}>Play</a>
+        </nav>
+      </div>
+    </header>
+  );
+};
