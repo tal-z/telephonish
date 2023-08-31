8d76e05c1e0f1ef146b6e1d7df2df4f75123e29d
talzaken <talzaken@gmail.com>
2023-08-30 21:21:12 -0400

    highlight client player in list

--- src/components/PlayerList.tsx
+++ src/components/PlayerList.tsx
@@ -3,15 +3,22 @@ import styles from "../styles/PlayerList.module.css";
 
 import { PlayerListItem } from "./PlayerListItem";
 
-export const PlayerList = ({ players }) => {
+export const PlayerList = ({ players, clientId }) => {
   const playerEntries = Object.entries(players);
+
+  console.log(playerEntries);
 
   return (
     <div className={styles.playerList}>
       <h2>Players</h2>
       <div className={styles.playerListInner}>
         {playerEntries.map(([id, player]) => (
-          <PlayerListItem key={id} id={id} name={player.name} />
+          <PlayerListItem
+            key={id}
+            id={id}
+            name={player.name}
+            isClient={id == clientId} // Add isClient prop
+          />
         ))}
       </div>
     </div>
--- src/components/PlayerListItem.tsx
+++ src/components/PlayerListItem.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import styles from "../styles/PlayerList.module.css";
-import { Player } from "./PlayerList";
 
-export const PlayerListItem = ({ id, name }: Player) => {
+export const PlayerListItem = ({ id, name, isClient }) => {
+  const listItemClassName = isClient
+    ? `${styles.playerListItem} ${styles.client}` // Add client class
+    : styles.playerListItem;
+
   return (
-    <li key={id} className={styles.playerListItem}>
+    <li key={id} className={listItemClassName}>
       <div className={styles.player}>
         <span className={styles.name}>{name}</span>
       </div>
